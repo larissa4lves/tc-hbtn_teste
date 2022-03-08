d7685d5f65b270c7278fd8f04e59044c2c2c1272
P4NDAPOWER <79761270+larissa4lves@users.noreply.github.com>
2022-03-08 16:23:36 -0300

    Add files via upload

---
+++ 0x01/calculo/OperacaoBasica.java
@@ -0,0 +1,42 @@
+public class OperacaoBasica {
+
+	public static double somar(double a, double b) {
+
+		return a + b;
+	}
+
+	public static double subtrair(double a, double b) {
+
+		return a - b;
+	}
+
+	public static double multiplicar(double a, double b) {
+
+		return a * b;
+	}
+
+	public static double dividir(double a, double b) {
+
+		return a / b;
+	}
+	
+	public static double sqrt(double a) {
+		return Math.sqrt(a);
+	}
+
+	public static String numero_primo(int numero) {
+	
+		if (numero <= 1 ) {
+			return "Número invalido. Numero menor ou igual a 1 não é primo";
+	}
+		if (numero % numero == 0) {
+			return "Numero válido, o numero é primo";
+		}
+		
+		else {
+			return "Número válido, mas o numero não é primo";
+		}
+			
+	}
+
+}
